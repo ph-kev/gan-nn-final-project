98d68e701aa6f5c65c5f885ae9959aa54bd110f3
ph-kev <kphan@g.hmc.edu>
2024-05-02 19:17:41 -0700

    Updated WGAN with loss tracking

--- WGAN.py
+++ WGAN.py
@@ -3,6 +3,7 @@ import torch
 import torchvision
 from torchvision import transforms
 import torch.nn as nn
+import pickle 
 
 from nn_helper import *
 
@@ -21,19 +22,33 @@ print(f"Using '{device}' device.")
 
 gen_input_size = 100 
 image_size = (28, 28)
-n_filters = 32 
+n_filters_gen = 128
+n_filters_dis = 128
 
-generator = Generator(100, n_filters)
-discriminator = Discriminator(n_filters)
+generator = Generator(100, n_filters_gen)
+discriminator = Discriminator(n_filters_dis)
 
 generator.to(device)
 discriminator.to(device)
 
 gen_optimizer = torch.optim.RMSprop(generator.parameters(), lr = 5e-5)
 dis_optimizer = torch.optim.RMSprop(discriminator.parameters(), lr = 5e-5)
+# gen_optimizer = torch.optim.Adam(generator.parameters(), lr = 0.0002, betas=(0.5, 0.999))
+# dis_optimizer = torch.optim.Adam(discriminator.parameters(), lr = 0.0002, betas=(0.5, 0.999))
 
-num_epoch = 500
-for curr_epoch in range(num_epoch):
+num_epoch = 100
+
+# Keep track of loss for discriminator and generator 
+dis_loss_arr = []
+gen_loss_arr = []
+dis_real_arr = []
+dis_fake_arr = []
+
+for curr_epoch in range(1, num_epoch + 1):
+    dis_loss_list = []
+    gen_loss_list = []
+    dis_real_list = []
+    dis_fake_list = [] 
     for batch_real_image, _ in data_loader:
         num_batch_real_image = batch_real_image.shape[0]
         for i in range(0, 5):
@@ -42,17 +57,17 @@ for curr_epoch in range(num_epoch):
 
             # Calculate probabilities for real images
             batch_real_image = batch_real_image.to(device)
-            dis_real_probs = discriminator(batch_real_image)
+            real_probs = discriminator(batch_real_image)
             
             # Calculate probabilities for fake images 
             dis_noise = create_noise(num_batch_real_image, gen_input_size).to(device)
             batch_fake_image = generator(dis_noise)
-            dis_fake_probs = discriminator(batch_fake_image)
-            dis_loss = wgan_discriminator_loss(dis_real_probs, dis_fake_probs)
+            fake_probs = discriminator(batch_fake_image)
+            dis_loss = wgan_discriminator_loss(real_probs, fake_probs)
             dis_loss.backward()
             dis_optimizer.step()
             for param in discriminator.parameters():
-                param.data.clamp_(-0.01, 0.01)
+                param.data.clamp_(-0.02, 0.02)
 
         # Training generator 
         generator.zero_grad()
@@ -67,8 +82,27 @@ for curr_epoch in range(num_epoch):
         # Print useful information 
         dis_loss_item = dis_loss.item()
         gen_loss_item = gen_loss.item()
-        print(f"Current epoch: {curr_epoch} | Dis_Loss: {dis_loss_item:3f} | Gen_Loss: {gen_loss_item:3f}")
+        dis_loss_list.append(dis_loss_item)
+        gen_loss_list.append(gen_loss_item) 
+
+        # Get probabilities 
+        dis_real_list.append(real_probs.mean().item())
+        dis_fake_list.append(fake_probs.mean().item())
         del batch_real_image, gen_noise, dis_noise
+    # Compute the average loss of the discriminator and generator and probs of discriminator 
+    dis_loss = np.mean(dis_loss_list)
+    gen_loss = np.mean(gen_loss_list)
+    dis_real = np.mean(dis_real_list)
+    dis_fake = np.mean(dis_fake_list)
+
+
+    # Append information to list 
+    dis_loss_arr.append(dis_loss)
+    gen_loss_arr.append(gen_loss)
+    dis_real_arr.append(dis_real)
+    dis_fake_arr.append(dis_fake) 
+    # Print useful information 
+    print(f"Current epoch: {curr_epoch} | Dis_Loss: {dis_loss:3f} | Gen_Loss: {gen_loss:3f}")
     # Save parameters every 5 epochs 
     # Save parameters every 1, 2, 5, 10, and 50 epochs 
     if curr_epoch == 1: 
@@ -90,3 +124,16 @@ for curr_epoch in range(num_epoch):
 # Save parameters after training is finished 
 torch.save(discriminator.state_dict(), "WGAN_params/dis-params-" + str(num_epoch))
 torch.save(generator.state_dict(), "WGAN_params/gen-params-" + str(num_epoch))
+
+# Save lists after training 
+with open('saved_losses/wgan_dis_loss_arr.pkl', 'wb') as f:
+    pickle.dump(dis_loss_arr, f)
+
+with open('saved_losses/wgan_gen_loss_arr.pkl', 'wb') as f:
+    pickle.dump(gen_loss_arr, f)
+
+with open('saved_losses/wgan_dis_real_arr.pkl', 'wb') as f:
+    pickle.dump(dis_real_arr, f)
+
+with open('saved_losses/wgan_dis_fake_arr.pkl', 'wb') as f:
+    pickle.dump(dis_fake_arr, f)
