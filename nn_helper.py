035b60ef5e647804139c203fe9629c637f0f519e
ph-kev <kphan@g.hmc.edu>
2024-05-03 16:52:04 -0700

    Added comments to helper file

--- nn_helper.py
+++ nn_helper.py
@@ -4,13 +4,17 @@ import torchvision
 from torchvision import transforms
 import torch.nn as nn
 
+# Set random seed 
 torch.manual_seed(1)
 np.random.seed(1)
 
 # Define class for generator and discriminator 
 # https://arxiv.org/pdf/1511.06434.pdf 
 # The paper about deep convolutional generative adversarial networks (DCGAN) give 
-# advice about making DCGANs. It is basically the same template as here. 
+# advice about making DCGANs which are the same as neural network architecture below. 
+# Additional resources used that were helpful for making NN architecture: 
+# https://pytorch.org/tutorials/beginner/dcgan_faces_tutorial.html
+# Machine Learnign with PyTorch and Scikit-Learn by Sebastian Raschka et al.
 class Generator(nn.Module):
     def __init__(self, input_size, n_filters):
         super().__init__()
@@ -56,30 +60,30 @@ class Discriminator(nn.Module):
         output = self.network(x)
         return output.view(-1, 1).squeeze(0) 
   
-# Generate noise from normal distribution 
+# Generate noise from normal distribution of mean 0 ana standard deviation 1
 def create_noise(batch_size, z_size):
     input_z = torch.normal(0.0, 1.0, (batch_size, z_size, 1, 1))
     return input_z
 
-# The loss function is for GAN 
+# All loss functions are adjusted so that the optimizer need to minimize and not maximize 
+# Loss function for GAN discriminator 
 def gan_discriminator_loss(real_output, fake_output):
-  """Added negative sign to minimize instead of maxmize"""
   num_samples = real_output.shape[0]
   loss = -(1 / num_samples) * torch.sum( torch.log(real_output) + (torch.log(1.0 - fake_output)))
   return loss
 
+# Loss function for GAN generator 
 def gan_generator_loss(fake_output):
-  """Added negative sign to minimize instead of maxmize"""
   num_fake_samples = fake_output.shape[0]
   loss = -(1 / num_fake_samples) * torch.sum((torch.log(fake_output)))
   return loss 
 
-# The loss function is for WGAN 
+# Loss function for WGAN discriminator 
 def wgan_discriminator_loss(real_output, fake_output):
-  '''Added negative sign to minimize instead of maximize'''
   loss = -(real_output.mean() - fake_output.mean())
   return loss
 
+# Loss function for WGAN generator 
 def wgan_generator_loss(fake_output):
   loss = -fake_output.mean()
   return loss 
